f98c51b3a5f9f7ebcecaab886586fbe450200b68
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2022-03-19 19:03:56 +0700

    push pertama sebelum melengkapi tampilan activity

---
+++ app/src/main/java/xyz/heydarrn/dynamicgithubuserapp/views/DetailOfUserActivity.kt
@@ -0,0 +1,12 @@
+package xyz.heydarrn.dynamicgithubuserapp.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.heydarrn.dynamicgithubuserapp.R
+
+class DetailOfUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_of_user)
+    }
+}
